588876d3398b8259a2bcfa05fbbbeaf24e207017
Zhipeng Zeng <2269225970+zzpgithub@users.noreply.github.com>
2018-06-03 14:24:40 +0800

    first commit

---
+++ src/main/java/com/tw/JxMarket/JxMarketApplication.java
@@ -0,0 +1,12 @@
+package com.tw.JxMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxMarketApplication.class, args);
+	}
+}
